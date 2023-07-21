59e1086efc461849dfc32abd1ba070b113b6d72c
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-21 19:16:52 +0800

    done

---
+++ src/Author.java
@@ -0,0 +1,25 @@
+public class Author {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NoSuchFieldException {
+        Article article = new Article();
+        Author author = new Author();
+        Class[] classes = {article.getClass(), author.getClass()};
+
+        for (var clazz : classes) {
+            Field[] declaredFields = clazz.getDeclaredFields();
+            Method[] declaredMethods = clazz.getDeclaredMethods();
+
+            for (Field field : declaredFields) {
+                int modifiers = field.getModifiers();
+                if (!Modifier.isPrivate(modifiers))
+                    System.out.println("Field with the name " + field.getName() + " should be private");
+
+                boolean hasSetter = Arrays.stream(declaredMethods)
+                        .anyMatch(method -> method.getName().equals("set" + capitalize(field.getName())));
+                boolean hasGetter = Arrays.stream(declaredMethods)
+                        .anyMatch(method -> method.getName().equals("get" + capitalize(field.getName())));
+
+                if (!hasSetter) System.out.println(clazz.getName() + " has no setter for " + field.getName());
+                if(!hasGetter) System.out.println(clazz.getName() + " has no getter for " + field.getName());
+            }
+        }
     }
+
+    private static String capitalize(String str) {
+        return str.substring(0,1).toUpperCase() + str.substring(1);
+    }
+
 }
